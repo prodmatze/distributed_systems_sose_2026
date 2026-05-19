a718e7c51a44ec6705545e6b0bacdb4751ac726f
Mathieu Wassmuth <prod.matze@gmail.com>
2026-05-19 19:11:33 +0200

    first api endpint /api/health

--- backend/api/src/api/main.py
+++ backend/api/src/api/main.py
@@ -2,6 +2,16 @@ from fastapi import FastAPI
 
 app = FastAPI(title="Chorus API")
 
+@app.get('/api/health')
+async def health_check():
+    dicc = {'ok': True}
+    return dicc
+
+@app.get('/api/channels')
+async def get_channels():
+    channels = ['Channel_1', 'Channel_2', 'Channel_3']
+    return {'channels': channels}
+
 
 # TODO(human): write the healthz endpoint here.
 # See the "Learn by Doing" message in the conversation for guidance.
